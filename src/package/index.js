abbb5ec520faf445366472ea2430e5002f583457
pengbo <pengbo@hopefound.cn>
2022-10-11 18:31:03 +0800

    11

--- src/package/index.js
+++ src/package/index.js
@@ -1,12 +1,16 @@
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import button from "./button";
 
 const coms = [button];
 
 // 批量组件注册
 const install = function (Vue) {
+  Vue.use(ElementUI);
   coms.forEach((com) => {
     Vue.component(com.name, com);
   });
 };
+
 
 export default install;
