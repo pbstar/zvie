c588c4aaf28e2ffaac45283e4014273e8045e6c8
pengbo <pengbo@hopefound.cn>
2022-10-11 16:44:14 +0800

    1

---
+++ src/package/button/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-button type="primary">主要de按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'zvi-button',
+  props: {
+    msg: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
--- src/views/doc/post/index.vue
+++ src/views/doc/post/index.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
     1
+    <zvi-botton></zvi-botton>
   </div>
 </template>
 <script>
+import zviBotton from '@/package/button'
 export default {
-  name: 'docIndex'
+  name: 'docIndex',
+  components: {
+    zviBotton
+  }
 }
 </script>
