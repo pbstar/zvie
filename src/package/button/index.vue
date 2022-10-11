d69e3e9f7753d43692c03f85ebadce8890247ab4
pengbo <pbstar@sina.cn>
2022-10-12 01:47:56 +0800

    1

--- src/package/button/index.vue
+++ src/package/button/index.vue
@@ -1,20 +1,22 @@
 <template>
   <div>
-    <el-button type="primary">主要de按钮</el-button>
+    <el-button class="bt" type="primary">主要de按钮</el-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'zvi-button',
+  name: "zvi-button",
   props: {
     msg: {
       type: String,
-      default: ''
-    }
-  }
-}
+      default: "",
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
-
+.bt {
+  color: red;
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
   lintOnSave: false,
+  publicPath: "",
 });
