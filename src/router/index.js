f6873844f6d56e37147428c3082d01bbd1867dbb
pengbo <pbstar@sina.cn>
2022-10-12 02:18:40 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,34 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import doc from '../views/doc/index.vue'
-import docIndex from '../views/doc/post/index.vue'
-Vue.use(VueRouter)
+import Vue from "vue";
+import VueRouter from "vue-router";
+import doc from "../views/doc/index.vue";
+import docIndex from "../views/doc/post/index.vue";
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'index',
-    redirect: "doc",
-  }, {
-    path: '/doc',
-    name: 'doc',
+    path: "/",
+    name: "index",
+    redirect: "/doc",
+  },
+  {
+    path: "/doc",
+    name: "doc",
     component: doc,
-    redirect: "doc/index",
+    redirect: "/doc/index",
     children: [
       {
-        path: 'index',
-        name: 'docIndex',
-        component: docIndex
-      }
-    ]
-  }
-]
+        path: "index",
+        name: "docIndex",
+        component: docIndex,
+      },
+    ],
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "hash",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
