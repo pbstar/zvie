622d53fe36760da60b75284a6279d4d8cc5ac0fe
pengbo <pengbo@hopefound.cn>
2022-10-12 18:16:24 +0800

    1

--- src/components/index/lefter/index.vue
+++ src/components/index/lefter/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="box">
-    <h3>开发指南</h3>
-    <p>安装</p>
-    <p>快速上手</p>
-    <h3>基础组件</h3>
-    <p>Button 按钮</p>
+    <h4>开发指南</h4>
+    <router-link to="/doc/install" :class="$route.name=='docInstall'?'activeLink':'link'">安装</router-link>
+    <router-link to="/doc/start" :class="$route.name=='docStart'?'activeLink':'link'">快速上手</router-link>
+    <h4>基础组件</h4>
+    <router-link to="/doc/button" :class="$route.name=='docButton'?'activeLink':'link'">Button 按钮</router-link>
   </div>
 </template>
 
@@ -16,6 +16,12 @@ export default {
       type: String,
       default: ''
     }
+  },
+  data() {
+    return {
+    }
+  },
+  created() {
   }
 }
 </script>
@@ -25,12 +31,40 @@ export default {
   display: flex;
   flex-direction: column;
 
-  h3 {
+  h4 {
     border-bottom: 1px solid #eee;
     height: 60px;
     line-height: 60px;
-    color: #888;
+    color: #666;
     font-weight: 400;
+  }
+
+  .link {
+    line-height: 35px;
+    height: 35px;
+    margin-top: 6px;
+    padding: 0 8px;
+    border-radius: 5px;
+    color: #888;
+    font-size: 15px;
+    text-decoration: none;
+  }
+
+  .activeLink {
+    line-height: 35px;
+    height: 35px;
+    margin-top: 6px;
+    padding: 0 8px;
+    border-radius: 5px;
+    color: #0b8586;
+    background-color: #eee;
+    font-size: 15px;
+    text-decoration: none;
+  }
+
+  .link:hover {
+    color: #0b8586;
+    background-color: #eee;
   }
 }
 </style>
