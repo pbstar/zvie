97b93b66a16f203582d314292a3c1edf1db37d99
pengbo <pengbo@hopefound.cn>
2022-10-19 18:18:23 +0800

    1

--- src/views/doc/index.vue
+++ src/views/doc/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="box">
-    <Lefter class="bleft"></Lefter>
+    <Lefter class="bleft" v-show="!isLeftFixed"></Lefter>
+    <div class="blMer" v-show="isLeftFixed"></div>
     <router-view class="bright" />
   </div>
 </template>
@@ -12,6 +13,43 @@ export default {
   components: {
     Lefter,
   },
+  data() {
+    return {
+      isLeftFixed: false
+    }
+  },
+  mounted() {
+    window.addEventListener("scroll", this.throttle(this.handleScroll, 50), false)
+  },
+  methods: {
+    handleScroll() {
+      let showH = document.documentElement.clientHeight
+      let showT = parseInt(document.documentElement.scrollTop)
+      let zHeight = document.body.scrollHeight
+      console.log(showH, showT, zHeight);
+      if (showT > 68) {
+        this.isLeftFixed = true
+      } else if () {
+        this.isLeftFixed = false
+      } else {
+
+      }
+    },
+
+    throttle(fn, wait) {
+      var timer = null;
+      return function () {
+        var context = this;
+        var args = arguments;
+        if (!timer) {
+          timer = setTimeout(function () {
+            fn.apply(context, args);
+            timer = null;
+          }, wait)
+        }
+      }
+    },
+  }
 };
 </script>
 <style lang="scss" scoped>
@@ -24,6 +62,10 @@ export default {
   .bleft {
     width: 180px;
     // position: fixed;
+  }
+
+  .blMer {
+    width: 180px;
   }
 
   .bright {
